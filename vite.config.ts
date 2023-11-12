9fcd5a551c227017354834dc4d4b47924f1df7d4
German Eichemberger <germaneichemberger@gmail.com>
2023-11-12 12:26:50 -0300

    adds wasm support

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 import { VitePluginFonts } from 'vite-plugin-fonts'
 import Components from 'unplugin-vue-components/vite'
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +20,12 @@ export default defineConfig({
         }
       }),
       Components(),
+      wasm(),
+      topLevelAwait(),
   ],
+  optimizeDeps: {
+    exclude: ["@silvia-odwyer/photon"],
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
